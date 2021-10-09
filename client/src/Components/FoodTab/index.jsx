84047c6ae61a0f3948eaf69adf17aa370a6141e9
Akarsh Lahariya <akarshlahariya1234@gmail.com>
2021-10-10 00:09:37 +0530

    add icon link

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -1,88 +1,147 @@
-import React from 'react'
-import { icons } from 'react-icons/lib';
-import {RiShoppingBag3Line} from "react-icons/ri"
-import {IoFastFoodOutline, IoNutritionOutline} from "react-icons/io5"
-import {BiDrink} from "react-icons/bi"
+import React, { useState, useEffect } from "react";
+import { useParams, Link  } from "react-router-dom";
+import { RiShoppingBag3Line } from "react-icons/ri";
+import { IoFastFoodOutline, IoNutritionOutline } from "react-icons/io5";
+import { BiDrink } from "react-icons/bi";
 
-const MobileTabs =() => {
-    return(<>
-        <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between md:justify-evenly text-gray-500 border">
-        <div className="flex flex-col items-center text-xl">
-        <RiShoppingBag3Line/>
-        <h5 className="text-sm">Delivery</h5>
+const MobileTabs = () => {
+  const [ allTypes, setAllTypes ] = useState([
+    {
+      id: "delivery",
+      icon: <RiShoppingBag3Line />,
+      name: "Delivery",
+      isActive: false, // isActive is false because it will re-render again and again so it will become true false true false that's why useEffect Hook will toggle it.
+    },
+    {
+      id: "dining",
+      icon: <IoFastFoodOutline />,
+      name: "Dining Out",
+      isActive: false,
+    },
+    {
+      id: "night",
+      icon: <BiDrink />,
+      name: "Nightlife",
+      isActive: false,
+    },
+    {
+      id: "nutri",
+      icon: <IoNutritionOutline />,
+      name: "Nutrition",
+      isActive: false,
+    },
+  ]);
+  const { type } = useParams();
+
+  // We can use useEffect to get the pparticular id
+  // useEffect(() => {
+  //   if (type) {
+  //     const updatTypes = allTypes.map((item) => {
+  //       if (item.id === type) {
+  //         return { ...item, isActive: true };
+  //       }
+
+  //       return item;
+  //     });
+
+  //     setAllTypes(updatTypes);
+  //   }
+  // }, [type]);
+
+  return (
+    <>
+      <div className="lg:hidden  bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between md:justify-evenly text-gray-500 border">
+        
+        {
+            allTypes.map((items) => (
+              <Link to={`/${items.id}`}>
+               <div
+                  className= { type === items.id ? " relative flex flex-col items-center text-xl text-zomato-400  "
+                                            :  "flex flex-col items-center text-xl "
+                                          }
+                                      >
+                <div className={type === items.id && "absolute -top-3 w-full h-2 border-t-2 border-zomato-400 "} />
+                {items.icon}
+                <h5 className="text-sm">{items.name}</h5>
+            </div>
+            </Link>
+            ))
+        }
+        {/* <div className="flex flex-col items-center text-xl">
+          <IoFastFoodOutline />
+          <h5 className="text-sm">Dining </h5>
         </div>
         <div className="flex flex-col items-center text-xl">
-        <IoFastFoodOutline/>
-        <h5 className="text-sm">Dining </h5>
+          <BiDrink />
+          <h5 className="text-sm">Night life</h5>
         </div>
         <div className="flex flex-col items-center text-xl">
-        <BiDrink/>
-        <h5 className="text-sm">Night life</h5>
-        </div>
-        <div className="flex flex-col items-center text-xl">
-        <IoNutritionOutline/>
-        <h5 className="text-sm">Nutrition</h5>
-        </div>
-        </div>
-    </>)
+          <IoNutritionOutline />
+          <h5 className="text-sm">Nutrition</h5>
+        </div> */}
+      </div>
+    </>
+  );
 };
 
 const LargeTabs = () => {
-    return (<>
-        <div className="hidden lg:flex container gap-14 px-20 mx-auto">
-            <div className="flex items-center gap-3">
-                <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                    <img 
-                        src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
-                        alt="delivery"
-                        className="w-full h-full"
-                    />
-                </div>
-                <h3 className="text-xl text-gray-700">Delivery</h3>
-            </div>
-            <div className="flex items-center gap-3">
-                <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                    <img 
-                        src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png"
-                        alt="delivery"
-                        className="w-full h-full"
-                    />
-                </div>
-                <h3 className="text-xl text-gray-700">Dining Out</h3>
-            </div>
-            <div className="flex items-center gap-3">
-                <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                    <img 
-                        src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png"
-                        alt="delivery"
-                        className="w-full h-full"
-                    />
-                </div>
-                <h3 className="text-xl text-gray-700">Nightlife</h3>
-            </div>
-            <div className="flex items-center gap-3">
-                <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                    <img 
-                        src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png"                        alt="delivery"
-                        className="w-full h-full"
-                    />
-                </div>
-                <h3 className="text-xl text-gray-700">Nutrition</h3>
-            </div>
+  return (
+    <>
+      <div className="hidden lg:flex container gap-14 px-20 mx-auto">
+        <div className="flex items-center gap-3">
+          <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
+            <img
+              src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
+              alt="delivery"
+              className="w-full h-full"
+            />
+          </div>
+          <h3 className="text-xl text-gray-700">Delivery</h3>
         </div>
-    </>)
+        <div className="flex items-center gap-3">
+          <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
+            <img
+              src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png"
+              alt="delivery"
+              className="w-full h-full"
+            />
+          </div>
+          <h3 className="text-xl text-gray-700">Dining Out</h3>
+        </div>
+        <div className="flex items-center gap-3">
+          <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
+            <img
+              src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png"
+              alt="delivery"
+              className="w-full h-full"
+            />
+          </div>
+          <h3 className="text-xl text-gray-700">Nightlife</h3>
+        </div>
+        <div className="flex items-center gap-3">
+          <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
+            <img
+              src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png"
+              alt="delivery"
+              className="w-full h-full"
+            />
+          </div>
+          <h3 className="text-xl text-gray-700">Nutrition</h3>
+        </div>
+      </div>
+    </>
+  );
 };
 
-const FoodTab  = () => {
-    return (
-        <>
-        <div>
-           <MobileTabs /> 
-           <LargeTabs />
-        </div>
-        </>
-    )
-}
+const FoodTab = () => {
+  return (
+    <>
+      <div>
+        <MobileTabs />
+        <LargeTabs />
+      </div>
+    </>
+  );
+};
 
-
-export default FoodTab 
+export default FoodTab;
