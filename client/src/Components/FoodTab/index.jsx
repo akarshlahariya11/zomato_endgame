82785f629fdc9496ba3d9cfa4821d9d5977c10a0
Akarsh Lahariya <akarshlahariya1234@gmail.com>
2021-10-10 16:58:45 +0530

    Large Screen added

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link  } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { RiShoppingBag3Line } from "react-icons/ri";
-import { IoFastFoodOutline, IoNutritionOutline } from "react-icons/io5";
+import { IoFastFoodOutline, IoNutritionOutline, IoThermometerSharp } from "react-icons/io5";
 import { BiDrink } from "react-icons/bi";
+import classnames from "classnames";
 
 const MobileTabs = () => {
-  const [ allTypes, setAllTypes ] = useState([
+  const [allTypes, setAllTypes] = useState([
     {
       id: "delivery",
       icon: <RiShoppingBag3Line />,
@@ -51,54 +52,98 @@ const MobileTabs = () => {
   return (
     <>
       <div className="lg:hidden  bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between md:justify-evenly text-gray-500 border">
-        
-        {
-            allTypes.map((items) => (
-              <Link to={`/${items.id}`}>
-               <div
-                  className= { type === items.id ? " relative flex flex-col items-center text-xl text-zomato-400  "
-                                            :  "flex flex-col items-center text-xl "
-                                          }
-                                      >
-                <div className={type === items.id && "absolute -top-3 w-full h-2 border-t-2 border-zomato-400 "} />
-                {items.icon}
-                <h5 className="text-sm">{items.name}</h5>
+        {allTypes.map((items) => (
+          <Link to={`/${items.id}`}>
+            <div
+              className={
+                type === items.id
+                  ? " relative flex flex-col items-center text-xl text-zomato-400  "
+                  : "flex flex-col items-center text-xl "
+              }
+            >
+              <div
+                className={
+                  type === items.id &&
+                  "absolute -top-3 w-full h-2 border-t-2 border-zomato-400 "
+                }
+              />
+              {items.icon}
+              <h5 className="text-sm">{items.name}</h5>
             </div>
-            </Link>
-            ))
-        }
-        {/* <div className="flex flex-col items-center text-xl">
-          <IoFastFoodOutline />
-          <h5 className="text-sm">Dining </h5>
-        </div>
-        <div className="flex flex-col items-center text-xl">
-          <BiDrink />
-          <h5 className="text-sm">Night life</h5>
-        </div>
-        <div className="flex flex-col items-center text-xl">
-          <IoNutritionOutline />
-          <h5 className="text-sm">Nutrition</h5>
-        </div> */}
+          </Link>
+        ))}
       </div>
     </>
   );
 };
 
 const LargeTabs = () => {
+  const [allTypes, setAllTypes] = useState([
+    {
+      id: "delivery",
+      imageDefault:"https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png",
+      imageActive:"https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png?output-format=webp",
+      name: "Delivery",
+      isActive: false, // isActive is false because it will re-render again and again so it will become true false true false that's why useEffect Hook will toggle it.
+      activeColor: "yellow",
+    },
+    {
+      id: "dining",
+      imageDefault:"https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png",
+      imageActive:"https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png",
+      name: "Dining Out",
+      isActive: false,
+      activeColor: "blue",
+    },
+    {
+      id: "night",
+      imageDefault:"https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png",
+      imageActive:"https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png",
+      name: "Nightlife",
+      isActive: false,
+      activeColor: "purple",
+    },
+    {
+      id: "nutri",
+      imageDefault:"https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png",
+      imageActive: "https://b.zmtcdn.com/data/o2_assets/0f6dcb1aef93fa03ea3f91f37918f3bc1616649503.png",
+      name: "Nutrition",
+      isActive: false,
+      activeColor: "yellow",
+    },
+  ]);
+
+  const { type } = useParams();
   return (
     <>
       <div className="hidden lg:flex container gap-14 px-20 mx-auto">
-        <div className="flex items-center gap-3">
-          <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
+
+        {allTypes.map((items)=> (
+          <Link to={`/${items.id}`}>
+          <div className={classnames(
+            "flex items-center gap-3 pb-2 transition duration-700 ease-in-out",
+            {
+              "border-b-2 border-zomato-400" : type === items.id,
+            } 
+          )}
+          >
+           <div className={classnames(
+                  "w-16 h-16 bg-gray-100 p-4 rounded-full",
+                  { [`bg-${items.activeColor}-100`]: type === items.id }
+                )}
+            >
             <img
-              src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
+              src={type === items.id ? items.imageActive : items.imageDefault}
               alt="delivery"
               className="w-full h-full"
             />
           </div>
-          <h3 className="text-xl text-gray-700">Delivery</h3>
+          <h3 className={type === items.id ? "text-xl text-zomato-400" : "text-xl text-gray-700"}>{items.name}</h3>
         </div>
-        <div className="flex items-center gap-3">
+        </Link>
+      ))}
+
+        {/* <div className="flex items-center gap-3">
           <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
             <img
               src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png"
@@ -127,7 +172,7 @@ const LargeTabs = () => {
             />
           </div>
           <h3 className="text-xl text-gray-700">Nutrition</h3>
-        </div>
+        </div> */}
       </div>
     </>
   );
